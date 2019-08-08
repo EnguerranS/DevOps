1f45fe85d2655ca7653ace4047ac09c16cee5549
Enguerran <Enguerran@192.168.1.13>
2019-08-08 12:57:32 +0200

    Test de poll Jenkins

---
+++ DevOps/src/test/java/Test/TestPoll.java
@@ -0,0 +1,17 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPoll {
+
+	@Test
+	public void test() {
+		int nombre1=2;
+		int nombre2=3;
+		
+		assertEquals(5, nombre1+nombre2);
+	}
+
+}
